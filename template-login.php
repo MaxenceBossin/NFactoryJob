32e754577e75dae09f1e14410e70c9070c1be57e
anthonychevalier <contactpro.antho@gmail.com>
2022-02-15 17:26:00 +0100

    login ok

---
+++ template-login.php
@@ -0,0 +1,29 @@
+<?php
+/*Template Name: Connexion*/
+get_header();
+?>
+
+<div class="wrap">
+    <h2>Connexion</h2>
+    <div class="box_img_login ">
+        <img src="//img de signup" alt="">
+    </div>
+    <form action="" method="get" class="form-example">
+                <div class="form-example">
+                <label for="name">Adresse mail </label>
+                <input type="email "name="email" id="email" required>
+            </div>
+                <div class="form-example">
+                <label for="email">Mot de passe </label>
+                <input type="password" name="password" id="password" required>
+            </div>
+            <a href="#">Mot de passe oublié</a>
+            <div class="form-button">
+                <input type="submit" value="Se connecter">
+            </div>
+            <a href="#">Je n'ai pas de comtpe</a>
+    </form>        
+</div>
+
+
+<?php get_footer();
