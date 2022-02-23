dc3d4a2bbc051e3adbfeea316a215437f7ee2a31
anthonychevalier <contactpro.antho@gmail.com>
2022-02-23 10:33:10 +0100

    404 html ok

--- 404.php
+++ 404.php
@@ -9,7 +9,16 @@
 
 get_header();
 ?>
+<head>
+    <title>404 not found</title>
+</head>
+<link rel="stylesheet" href="asset/css/style.css">
 
-	
+<div class="error-page">
+    <div>
+        <h1 data-h1="404">404</h1>
+        <p data-p="NOT FOUND">NOT FOUND</p>
+    </div>
+</div>	
 <?php
 get_footer();
