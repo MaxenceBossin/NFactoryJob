a54e5024e6b792dba2dfe67c13226bca86732f1a
anthonychevalier <contactpro.antho@gmail.com>
2022-02-23 10:16:19 +0100

    modif footer

--- footer.php
+++ footer.php
@@ -16,7 +16,9 @@
 		<div class="site-info">
 			<div id="contact">
 				<h2>Nous contacter</h2>
-				<p>Coordonnées</p>
+				<p>Téléphone : 02 35 70 04 04</p>
+				<p>Mail : contact@nfactoryjob.com</p>
+				<p>30 Place Henri Gadeau de Kerville,76100 Rouen</p>
 			</div>
 			<div id="maps">
 			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2594.862993344376!2d1.0790240159175741!3d49.43040416821315!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e0df1548cd768b%3A0x70b4b34959b1ec9f!2sNeed%20for%20School!5e0!3m2!1sfr!2sfr!4v1644920787766!5m2!1sfr!2sfr" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
