92edd23dbc06dcf5ce9f168630d5b084d4372af8
Leonard Jouen <leonardjouen@gmail.com>
2022-02-25 10:04:23 +0100

    patch erreurs page login

--- 404.php
+++ 404.php
@@ -9,8 +9,17 @@
 
 get_header();
 ?>
+<section id="errorpage">
+    <div class="message-box">
+        <h1>404</h1>
+        <p>Page not found</p>
+        <div class="buttons-con">
+            <div class="action-link-wrap">
+                <a class="link-button link-back-button">Back</a>
+            </div>
+        </div>
+    </div>
 
-	
-
+</section>
 <?php
 get_footer();
